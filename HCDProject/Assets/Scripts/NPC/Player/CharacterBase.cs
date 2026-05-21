b040d4b19c07ae314f0f5a12fa1c7ebba497ddbc
akfvhdl321 <akfvhdl321@gmail.com>
2026-05-21 16:32:59 +0900

    [ADD] CharacterBase 스크립트 추가
    직업 베이스 구조를 가진 최상위 클래스 생성
    
    - 캐릭터 스탯 용 데이터 클래스 생성
    - editorConfig 파일 추가(VS 인코딩 용도)

---
+++ HCDProject/Assets/Scripts/NPC/Player/CharacterBase.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+    protected StateMachine _stateMachine;
+
+    protected Transform _currentTarget;
+
+    protected Vector3 _homePosition;
+
+    protected CharacterStat _currentStats;
+
+    public abstract void Skill();
+
+    public virtual void Move(Vector3 targetPosition)
+    {
+
+    }
+
+    public virtual Transform FindNearEnemy()
+    {
+        return null;
+    }
+
+}
---
+++ HCDProject/Assets/Scripts/NPC/Player/CharacterStat.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class CharacterStat
+{
+    public int _maxHp;
+
+    public int _attackPower;
+
+    public int _defense;
+
+    public int _moveSpeed;
+
+    public float _attackSpeed;
+
+    public float _critRate;
+
+    public float _critDamage;
+}
