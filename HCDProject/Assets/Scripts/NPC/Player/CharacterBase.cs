baeb05f1a132d77a9b181db00bab80e423a0dc65
akfvhdl321 <akfvhdl321@gmail.com>
2026-05-22 17:35:16 +0900

    [ADD] IdleState 및 CharacterBase 기능 업데이트
    - 기존 캐릭터 Base 클래스 기능 추가
     - 적 탐색을 Base에 넘겨줌
    
    - Idle 패턴 추가

--- HCDProject/Assets/Scripts/NPC/Player/CharacterBase.cs
+++ HCDProject/Assets/Scripts/NPC/Player/CharacterBase.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 // 모든 직업군이 공통으로 가질 클래스
 public abstract class CharacterBase : MonoBehaviour
@@ -9,58 +10,62 @@ public abstract class CharacterBase : MonoBehaviour
 
     private IdlePlayerState _idlePlayerState;
 
-    protected Transform _currentTarget;
+    private ChasePlayerState _chasePlayerState;
+
+    private Transform _currentTarget; // 탐지할 대상
 
     private Vector3 _homePosition; // 지정된 위치
-    
+
+
+    private List<Collider2D> _enemyList = new List<Collider2D>(10);
+    private ContactFilter2D _layerFilter = new ContactFilter2D();
+
     [SerializeField] private CharacterBaseData _baseData;
 
     private CharacterStat _currentStats;
 
+    public Transform currentTarget
+    {
+        get => _currentTarget;
+       
+        set => _currentTarget = value;
+        
+    }
+
     public CharacterStat stat
     {
-        get
-        {
-            return _currentStats;
-        }
+        get => _currentStats;
     }
 
     public Vector3 homePosition
     {
-        get
-        {
-            return _homePosition;
-        }
+        get => _homePosition;
         
-        set
-        {
-            _homePosition = value;
-        }
+        set => _homePosition = value;
     }
 
     public StateMachine state
     {
-        get
-        {
-            return _stateMachine;
-        }
-
+        get => _stateMachine;
+        
     }
 
     public IdlePlayerState idle
     {
-        get
-        {
-            return _idlePlayerState;
-        }
+        get => _idlePlayerState;
+        
     }
 
     public SpawnPlayerState spawn
     {
-        get
-        {
-            return _spawnPlayerState;
-        }
+        get => _spawnPlayerState;
+        
+    }
+
+    public ChasePlayerState chase
+    {
+        get => _chasePlayerState;
+        
     }
 
     public void Awake()
@@ -68,7 +73,9 @@ public abstract class CharacterBase : MonoBehaviour
         _stateMachine = new StateMachine();
         _spawnPlayerState = new SpawnPlayerState(this);
         _idlePlayerState = new IdlePlayerState(this);
-        
+        _chasePlayerState = new ChasePlayerState(this);
+        _layerFilter.SetLayerMask(LayerMask.GetMask("Monster")); // 레이어 갖고옴
+        _layerFilter.useLayerMask = true;
     }
 
     public void Init(CharacterBaseData data)
@@ -97,6 +104,28 @@ public abstract class CharacterBase : MonoBehaviour
 
     public virtual Transform FindNearEnemy()
     {
+        int count = Physics2D.OverlapCircle(this.transform.position,
+            5f,
+            _layerFilter,
+            _enemyList);
+
+        if (count >= 1) // 탐지 수가 1명 이상인 경우.
+        {
+            float minDist = float.MaxValue;
+            Transform closet = null;
+            for (int i = 0; i < _enemyList.Count; i++) // 가장 가까운 적 탐색
+            {
+                float dist = Vector3.Distance(this.transform.position,
+                    _enemyList[i].transform.position);
+
+                if (dist <= minDist)
+                {
+                    minDist = dist;
+                    closet = _enemyList[i].transform;
+                }
+            }
+            return closet;
+        }
         return null;
     }
 
