b0132818995f70670921a9853e60371e0c714cc1
baewhv <baewhv@naver.com>
2026-05-23 16:50:40 +0900

    [FIX] : ITagetable 수정

--- HCDProject/Assets/Scripts/Interface/ITargetable.cs
+++ HCDProject/Assets/Scripts/Interface/ITargetable.cs
@@ -1,8 +1,13 @@
 ﻿//데미지를 줄 수 있는 함수
 //적용 대상 : 플레이 캐릭터, 몬스터, 벽
 
+
+using UnityEngine;
+
 public interface ITargetable
 {
+    public GameObject GetTargetObject { get; set; }
+    
     public void SetDamage();
     public void SetHeal();
 }
