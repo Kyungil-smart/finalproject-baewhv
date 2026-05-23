c54e608a721e8ed83b211c68987ec5e5c026111b
akfvhdl321 <akfvhdl321@gmail.com>
2026-05-23 14:55:29 +0900

    [ADD] AttackPlayerState 패턴 기능 추가
    - 공격 로직만 빼고 추가했음

---
+++ HCDProject/Assets/Scripts/NPC/Player/PlayerState/AttackPlayerState.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class AttackPlayerState : IState
+{
+    private CharacterBase _owner;
+    
+    float coolCount;
+
+    public AttackPlayerState(CharacterBase owner)
+    {
+        _owner = owner;
+    }
+
+    public void Enter()
+    {
+        coolCount = 0;
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+    public void Update()
+    {
+        if (_owner.currentTarget == null)
+        {
+            _owner.state.ChangeState(_owner.idle);
+            return;
+        }
+
+        float dist = Vector3.Distance(_owner.transform.position, _owner.currentTarget.position);
+
+        if (dist > _owner.stat._attackRange) // 공격 범위 벗어났으면
+        {
+            _owner.state.ChangeState(_owner.chase);
+            return;
+        }
+
+        if (coolCount <= _owner.stat._attackSpeed)
+        {
+            coolCount += Time.deltaTime;
+        }
+
+        else
+        {
+            // 공격
+            coolCount = 0;
+        }
+    }
+}
