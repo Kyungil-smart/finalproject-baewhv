5ecf7d1f9d9b4313ff344be0d20173a4beb3e525
baewhv <baewhv@naver.com>
2026-05-30 16:49:56 +0900

    [FIX] : 캐릭터 캔버스에 베이스 슬롯 부착

--- HCDProject/Assets/Scripts/UI/IngameBottomUIController.cs
+++ HCDProject/Assets/Scripts/UI/IngameBottomUIController.cs
@@ -1,12 +1,19 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UIElements;
 
 public class IngameBottomUIController : BaseUIController<IngameBottomUIController>
 {
-    [SerializeField] private GameObject SortPhaseUI;
-    [SerializeField] private GameObject BattlePhaseUI;
-    [SerializeField] private Slider WallHP;
+    [SerializeField] private GameObject sortPhaseUI;
+    [SerializeField] private GameObject battlePhaseUI;
+    [SerializeField] private Slider wallHP;
+    
+    
+    [SerializeField] private SlotBase[] characterSlots;
+    public SlotBase[] GetSlots => characterSlots;
+    
+        
     
     public bool IsSortPhase = false;
 
@@ -14,7 +21,6 @@ public class IngameBottomUIController : BaseUIController<IngameBottomUIControlle
     {
         
     }
-
     public void OnEndSort()
     {
         //아직 정렬 가능한 횟수가 남으면 팝업 출력.
@@ -23,19 +29,19 @@ public class IngameBottomUIController : BaseUIController<IngameBottomUIControlle
 
     public void SetWallHP(float value)
     {
-        WallHP.value = value;
+        wallHP.value = value;
     }
 
     public void SetSortPhase()
     {
-        SortPhaseUI.SetActive(true);
-        BattlePhaseUI.SetActive(false);
+        sortPhaseUI.SetActive(true);
+        battlePhaseUI.SetActive(false);
     }
 
     public void SetBattlePhase()
     {
-        SortPhaseUI.SetActive(false);
-        BattlePhaseUI.SetActive(true);
+        sortPhaseUI.SetActive(false);
+        battlePhaseUI.SetActive(true);
         
     }
 }
