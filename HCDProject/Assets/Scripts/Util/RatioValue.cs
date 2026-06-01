52172bf3c640d6fc0479a5c41432a585fe17c80f
baewhv <baewhv@naver.com>
2026-06-01 12:02:22 +0900

    [ADD] : 옵저버 벨류를 이용한 비율 밸류 클래스 추가.

---
+++ HCDProject/Assets/Scripts/Util/RatioValue.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class RatioIntValue : ObserveValue<int>
+{
+    [SerializeField] private int _maxValue;
+
+    private UnityEvent<float> OnRatioChange = new ();
+    public int MaxValue
+    {
+        get => _maxValue;
+        set
+        {
+            _maxValue = value;
+            OnRatioChange.Invoke(_data/(float)_maxValue);
+        }
+    }
+
+    public RatioIntValue(int maxValue)
+    {
+        _maxValue = maxValue;
+        AddListener(OnChangeValue);
+    }
+
+    public RatioIntValue(int maxValue, int currentValue) : this(maxValue)
+    {
+        _data = currentValue;
+    }
+
+    private void OnChangeValue(int value)
+    {
+        OnRatioChange.Invoke(_data/(float)_maxValue);
+    }
+}
+
+[Serializable]
+public class RatioFloatValue : ObserveValue<float>
+{
+    [SerializeField] private float _maxValue;
+
+    private UnityEvent<float> OnRatioChange = new ();
+    public float MaxValue
+    {
+        get => _maxValue;
+        set
+        {
+            _maxValue = value;
+            OnRatioChange.Invoke(_data/_maxValue);
+        }
+    }
+
+    public RatioFloatValue(float maxValue)
+    {
+        _maxValue = maxValue;
+        AddListener(OnChangeValue);
+    }
+
+    public RatioFloatValue(int maxValue, int currentValue) : this(maxValue)
+    {
+        _data = currentValue;
+    }
+
+    private void OnChangeValue(float value)
+    {
+        OnRatioChange.Invoke(_data/_maxValue);
+    }
+}
