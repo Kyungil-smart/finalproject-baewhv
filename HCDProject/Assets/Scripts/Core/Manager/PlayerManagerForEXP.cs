cc927056b59a8cc292f019926ac6e079c9b19eca
baewhv <baewhv@naver.com>
2026-06-09 13:08:21 +0900

    [ADD] : 플레이어 경험치 관리용 partial class 추가

---
+++ HCDProject/Assets/Scripts/Core/Manager/PlayerManagerForEXP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class PlayerManager
+{
+    private RatioIntValue exp;
+    private ObserveValue<int> level = new ();
+    private List<StoryExpRawData> LevelData;
+
+    private void Start()
+    {
+        StartEXP();
+    }
+
+    private void StartEXP()
+    {
+        GetEXPData();
+        level.Value = 1;
+        exp = new RatioIntValue((int)LevelData[level.Value-1].TOTAL_EXP, 0);
+        Debug.Log("here1");
+        if (Service.Get<UIManager>() && Service.Get<UIManager>().GetUI<IngameBottomUIController>())
+        {
+            level.AddListener(Service.Get<UIManager>().GetUI<IngameBottomUIController>().SetLevelText);
+            exp.AddValuesListener(Service.Get<UIManager>().GetUI<IngameBottomUIController>().SetExp);
+            Debug.Log("here2");
+            exp.Invoke();
+        }
+    }
+
+    private void GetEXPData()
+    {
+        if (Service.Get<DataManager>())
+            LevelData = Service.Get<DataManager>().StoryExpTable.data;
+    }
+
+    public void GetExp(int value)
+    {
+        exp.Value += value;
+    }
+}
