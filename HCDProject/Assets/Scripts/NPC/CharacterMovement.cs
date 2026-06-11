35e4103258ab2b0c51997e4c8b48efd51a0bd8f7
kamyou <example1130@naver.com>
2026-06-11 11:20:11 +0900

    [UPDATE] : CharacterMovement 스크립트 애니메이션 파라미터 변경 로직 추가
    + IdlePlayerState 정지 시 파라미턴 변경 함수 호출하도록 변경

--- HCDProject/Assets/Scripts/NPC/CharacterMovement.cs
+++ HCDProject/Assets/Scripts/NPC/CharacterMovement.cs
@@ -6,6 +6,9 @@ public class CharacterMovement : MonoBehaviour
     private BaseController _controller;
     public NavMeshAgent Agent { get; private set; }
 
+    private Animator _anim;
+    private Vector2 _lastDir;
+
     private bool _isMove = true;
     public bool IsMove => _isMove;
     
@@ -18,11 +21,19 @@ public class CharacterMovement : MonoBehaviour
         Agent.updateRotation = false;
         Agent.updateUpAxis = false;
         Agent.stoppingDistance = 0.1f;
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        AnimSet();
     }
 
     public void Stop()
     {
+        _anim.SetBool("Move", false);
         Agent.isStopped = true;
+        Agent.ResetPath();
     }
 
     public void Move(Vector2 target)
@@ -32,6 +43,27 @@ public class CharacterMovement : MonoBehaviour
         _currentTarget = target;
         
         Agent.isStopped = false;
+        
+        _anim.SetBool("Move", true);
+        
         Agent.SetDestination(target);
+    }
+
+    private void AnimSet()
+    {
+        Vector2 dir = Agent.velocity.normalized;
+
+        if (dir.sqrMagnitude > 0.01f)
+        {
+            _lastDir = dir;
+        }
+
+        _anim.SetFloat("MoveX", _lastDir.x);
+        _anim.SetFloat("MoveY", _lastDir.y);
+    }
+
+    public void MoveFalse()
+    {
+        _anim.SetBool("Move", false);
     }
 }
