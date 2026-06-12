befd420c3b6af09cf5d751e0d1093f29fbb6b7f5
baewhv <baewhv@naver.com>
2026-06-12 10:02:45 +0900

    [FIX] : 내러티브 씬 일단은 스킵할 수 있도록 수정.

--- HCDProject/Assets/Scripts/Core/Manager/NarrativeManager.cs
+++ HCDProject/Assets/Scripts/Core/Manager/NarrativeManager.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class NarrativeManager : MonoBehaviour
 {
     private NarrativeUIController ui;
-    
+
+    private void Start()
+    {
+        StartCoroutine(TempSkip());
+    }
+
+    private IEnumerator TempSkip()
+    {
+        yield return YieldContainer.WaitForSeconds(3.0f);
+        Service.Get<GameManager>()?.NarrativeEnd();
+    }
 }
