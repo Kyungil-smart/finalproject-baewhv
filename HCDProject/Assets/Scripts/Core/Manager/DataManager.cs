d000ae842619446acf86f0a3546691bb1a6daacc
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-06-16 15:56:28 +0900

    [UPDATE] 데이터 테이블에 TutorialTable추가

--- HCDProject/Assets/Scripts/Core/Manager/DataManager.cs
+++ HCDProject/Assets/Scripts/Core/Manager/DataManager.cs
@@ -24,6 +24,7 @@ public class DataManager : BaseManager<DataManager>
     public StaticValueTable StaticValueTable {get; private set;}
     public StoryExpTable StoryExpTable {get; private set;}
     public StoryStageTable StoryStageTable {get; private set;}
+    public TutorialTable TutorialTable {get; private set;}
 
     public RatioIntValue dataValue;
     
@@ -61,7 +62,8 @@ public class DataManager : BaseManager<DataManager>
             ("STORY_LOCALIZING_TABLE", json => StoryLocalizingTable = JsonUtility.FromJson<StoryLocalizingTable>(json)),
             ("STATIC_VALUE_TABLE", json => StaticValueTable = JsonUtility.FromJson<StaticValueTable>(json)),
             ("STORY_EXP_TABLE", json => StoryExpTable = JsonUtility.FromJson<StoryExpTable>(json)),
-            ("STORY_STAGE_TABLE", json => StoryStageTable = JsonUtility.FromJson<StoryStageTable>(json))
+            ("STORY_STAGE_TABLE", json => StoryStageTable = JsonUtility.FromJson<StoryStageTable>(json)),
+            ("TUTORIAL_TABLE", json => TutorialTable = JsonUtility.FromJson<TutorialTable>(json)),
         };
         
         // 총 데이터 파일의 개수 
