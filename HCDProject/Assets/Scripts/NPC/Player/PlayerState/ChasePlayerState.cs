4f2c0ff260998b4ff5ef9bd7408d542307a24c41
akfvhdl321 <akfvhdl321@gmail.com>
2026-06-17 16:54:05 +0900

    [FIX] 플레이어 추적패턴 변경
    기존에 예외처리 로직이 잘못되어 벡터값을 변경하였음

--- HCDProject/Assets/Scripts/NPC/Player/PlayerState/ChasePlayerState.cs
+++ HCDProject/Assets/Scripts/NPC/Player/PlayerState/ChasePlayerState.cs
@@ -25,21 +25,19 @@ public class ChasePlayerState : PlayerBaseState
             _owner.state.ChangeState(_owner.idle);
             return;
         }
-        float homeDist = Vector2.Distance(_owner.transform.position, _owner.homePosition);
-        if (homeDist > _owner.Stats._chaseRange)
+        float dist = Vector2.Distance(_owner.transform.position,
+            _owner.GetCurrentTarget.GetTargetObject.transform.position)
+            - _owner.GetRadius() - _owner.GetCurrentTarget.GetRadius();
+        //float homeDist = Vector2.Distance(_owner.transform.position, _owner.homePosition);
+        if (dist > _owner.Stats._chaseRange)
         {
             _owner.SetCurrentTarget(null);
             _owner.Movement.Stop();
             _owner.state.ChangeState(_owner.idle);
             return;
         }
+        
 
-        float dist = Vector3.Distance(_owner.transform.position,
-            _owner.GetCurrentTarget.GetTargetObject.transform.position)
-            - _owner.GetRadius() - _owner.GetCurrentTarget.GetRadius();
-
-        /*Debug.Log($"[Chase추적] {_owner.gameObject.name} | 적까지:{dist:F2} | 탐지범위:{_owner.Stats._chaseRange:F2}" +
-            $" | 원밖인데쫓는중? {(dist > _owner.Stats._chaseRange ? "★예★" : "아니오")}");*/
         if (dist <= _owner.CurrentSkillRange)
         {
             _owner.Movement.Stop();
