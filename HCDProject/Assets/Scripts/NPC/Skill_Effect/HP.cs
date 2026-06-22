e61a5ab99738ff37000736ad11e1474f6d3f07b6
kamyou <example1130@naver.com>
2026-06-22 13:14:12 +0900

    [UPDATE] : Skill HP 이펙트 사제 힐 로직 추가

--- HCDProject/Assets/Scripts/NPC/Skill_Effect/HP.cs
+++ HCDProject/Assets/Scripts/NPC/Skill_Effect/HP.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Burst.CompilerServices;
+﻿using JetBrains.Annotations;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class HP : BaseEffect
@@ -7,16 +8,27 @@ public class HP : BaseEffect
     {
         BaseSkill = baseSkill;
     }
-    
-    public override void ApplyEffect(BaseController user, ITargetable target, Skill skill, float value)
+
+    public override void ApplyEffect(BaseController user, ITargetable target, Skill skill)
     {
+        if (skill.SKILL_ABT_02 == ESkillAbilityType.ATTACK_BASE)
+        {
+            if (skill.SKILL_AT == ETargetType.ALLY)
+            {
+                // 사제 힐 로직
+                target.SetHeal((int)(user.Stats._attackPower * skill.SKILL_AB_02));
+            }
+
+            return;
+        }
+
         if (skill.SKILL_AT == ETargetType.ALLY)
         {
-            target.SetHeal((int)value);
+            target.SetHeal((int)skill.SKILL_AB_01);
         }
-        else if(skill.SKILL_AT == ETargetType.ENEMY)
+        else if (skill.SKILL_AT == ETargetType.ENEMY)
         {
-            target.SetDamage((int)value);
+            target.SetDamage((int)skill.SKILL_AB_01 * -1);
         }
     }
 }
