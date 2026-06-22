2b80b0c6d1d17ed7ffec8ced5e10fea89d6d70ae
kamyou <example1130@naver.com>
2026-06-22 13:14:31 +0900

    [ADD] : Skill ATK 이펙트 스크립트 생성

--- HCDProject/Assets/Scripts/NPC/Skill_Effect/ATK.cs
+++ HCDProject/Assets/Scripts/NPC/Skill_Effect/ATK.cs
@@ -1,6 +1,14 @@
 using UnityEngine;
 
-public class ATK
+public class ATK : BaseEffect
 {
-    
+    public ATK(BaseSkill baseSkill)
+    {
+        BaseSkill = baseSkill;
+    }
+
+    public override void ApplyEffect(BaseController user, ITargetable target, Skill skill)
+    {
+        
+    }
 }
