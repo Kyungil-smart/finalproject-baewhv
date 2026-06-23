82a83d4f77c4b63b9c8c7fa037eee9e0f7ac169e
akfvhdl321 <akfvhdl321@gmail.com>
2026-06-23 16:39:54 +0900

    [ADD] PlayerRelics 스크립트 생성
    - PlayerRelics 스크립트 생성
    - 기존 유물스킬 마법화살 로직 이관

---
+++ HCDProject/Assets/Scripts/NPC/Player/Relic/PlayerRelics.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerRelics : MonoBehaviour
+{
+    private BaseCharacter _character;
+
+    private void Awake()
+    {
+        _character = GetComponent<BaseCharacter>();
+    }
+
+    public void TryMagicBow(ITargetable target, Skill skill) // 마법 활
+    {
+        float rate = _character.PlayerStat._doubleAtkRate;
+        if (rate <= 0) return;
+
+        if(Random.value < rate)
+        {
+            target.SetDamage((int)(_character.Stats._attackPower * skill.SKILL_AB_01));
+        }
+    }
+}
