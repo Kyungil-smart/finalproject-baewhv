8ca4b57b304fa7b39bc653f9c1c4fd25ebcbff60
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-06-30 12:34:35 +0900

    [UPDATE] 클리어 실패시 재시작을 위한 추가

--- HCDProject/Assets/Scripts/Core/Manager/SceneController.cs
+++ HCDProject/Assets/Scripts/Core/Manager/SceneController.cs
@@ -97,7 +97,7 @@ public class SceneController : BaseManager<SceneController>
 
     private IEnumerator UnLoadActiveSceneRoutine(SceneType sceneType)
     {
-        if (sceneType == SceneType.ModeSelect || sceneType == SceneType.StageSelect || sceneType == SceneType.InGame || sceneType == SceneType.Tutorial || sceneType == SceneType.Narrative)
+        if (sceneType == SceneType.ModeSelect || sceneType == SceneType.StageSelect || sceneType == SceneType.InGame || sceneType == SceneType.Tutorial )
         {
             Scene targerScene = SceneManager.GetSceneByBuildIndex((int)sceneType);
             if (targerScene.isLoaded)
@@ -131,6 +131,5 @@ public enum SceneType
     ModeSelect = 1,
     StageSelect = 2,
     InGame = 3,
-    Tutorial = 4,
-    Narrative = 5
+    Tutorial = 4
 }
