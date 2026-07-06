d12bd6614126807e27df9b419db94aec828d325b
baewhv <baewhv@naver.com>
2026-07-06 09:59:07 +0900

    [FIX] : 팝업 UI 수정

--- HCDProject/Assets/Scripts/UI/StagePopUpUI.cs
+++ HCDProject/Assets/Scripts/UI/StagePopUpUI.cs
@@ -1,9 +1,10 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.Events;
 using UnityEngine.Localization.Components;
-using UnityEngine.Serialization;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.UI;
 
 public class StagePopUpUI : MonoBehaviour
@@ -12,15 +13,19 @@ public class StagePopUpUI : MonoBehaviour
     [SerializeField] private LocalizeStringEvent stageTypeText;
     [SerializeField] private GameObject imageLayout;
     [SerializeField] private Image stageImage;
-    [SerializeField] private Sprite TempCharacterImage;
-    [SerializeField] private Sprite TempStageImage;
+    [SerializeField] private Sprite tempCharacterImage;
+    [SerializeField] private Sprite tempStageImage;
+    [SerializeField] private Sprite tempBossImage;
 
-    [SerializeField] private TextMeshProUGUI EventTopText;
-    [SerializeField] private TextMeshProUGUI EventBottomText;
+    [SerializeField] private TextMeshProUGUI eventTopText;
+    [SerializeField] private TextMeshProUGUI eventBottomText;
     
-    [SerializeField] private GameObject MaintanenceLayout;
-    [SerializeField] private GameObject RelicLayout;
-    [SerializeField] private GameObject HPGaugeLayout;
+    [SerializeField] private GameObject bossInfoLayout;
+    [SerializeField] private Image bossSprite;
+    
+    [SerializeField] private GameObject maintenanceLayout;
+    [SerializeField] private GameObject relicLayout;
+    [SerializeField] private GameObject hpGaugeLayout;
         
     [SerializeField] private GameObject buttonLayout;
     [SerializeField] private Button positiveButton;
@@ -35,10 +40,12 @@ public class StagePopUpUI : MonoBehaviour
     {
         imageLayout.SetActive(false);
         buttonLayout.SetActive(false);
-        MaintanenceLayout.SetActive(false);
-        RelicLayout.SetActive(false); 
-        EventTopText.gameObject.SetActive(false);
-        EventBottomText.gameObject.SetActive(false);
+        maintenanceLayout.SetActive(false);
+        relicLayout.SetActive(false); 
+        eventTopText.gameObject.SetActive(false);
+        eventBottomText.gameObject.SetActive(false);
+        hpGaugeLayout.SetActive(false);
+        bossInfoLayout.SetActive(false);
     }
 
     public void OpenStagePopup(StoryStageRawData _data)
@@ -71,31 +78,45 @@ public class StagePopUpUI : MonoBehaviour
     private void SetEvent()
     {
         imageLayout.SetActive(true);
-        stageImage.sprite = TempCharacterImage;
+        stageImage.sprite = tempCharacterImage;
         
     }
 
     private void SetMaintenance()
     {
-        MaintanenceLayout.SetActive(true);
-        
+        maintenanceLayout.SetActive(true);
+        bossInfoLayout.SetActive(true);
+        var subData = Service.Get<DataManager>().StoryStageTable.data
+            .Find(x => x.CHAPTER == data.CHAPTER && x.STAGE == data.STAGE + 1);
+        Addressables.LoadAssetAsync<Sprite>(subData.BOSS_MONSTER_IMG_ID).Completed += (handle) =>
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                bossSprite.sprite = handle.Result;
+            }
+        };
     }
 
     private void SetNormal()
     {
         imageLayout.SetActive(true);
-        stageImage.sprite = TempStageImage;
-        
+        stageImage.sprite = tempStageImage;
+        hpGaugeLayout.SetActive(true);
         SetBottomButton(StageType.NORMAL_F, () => { }, () => { });
     }
 
     private void SetBoss()
     {
+        imageLayout.SetActive(true);
+        stageImage.sprite = tempBossImage;
+        hpGaugeLayout.SetActive(true);
         SetBottomButton(StageType.BOSS_F, () => { }, () => { });
     }
 
     private void SetRelic(bool isPresent)
     {
+        maintenanceLayout.SetActive(false);
+        bossInfoLayout.SetActive(false);
     }
 
     private void SetBottomButton(StageType type, UnityAction positive, UnityAction negative)
