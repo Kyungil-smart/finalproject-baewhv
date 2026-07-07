7ce5fee635b74dc1164ef05f6a487873e59e8ae8
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-07-07 14:51:23 +0900

    [UPDATE] 타임매니저에 전투중 시간 체크 추가

--- HCDProject/Assets/Scripts/Core/Manager/TimeManager.cs
+++ HCDProject/Assets/Scripts/Core/Manager/TimeManager.cs
@@ -11,6 +11,29 @@ public class TimeManager : BaseManager<TimeManager>
     
     public bool IsPaused => pauseCount > 0;
 
+    private float _battleTime = 0f;
+    public float BattleTime => _battleTime;
+
+    private void Update()
+    {
+        var gameManager = Service.Get<GameManager>();
+        if (gameManager != null && gameManager.CurrentState.Value == GameState.Wave)
+        {
+            UpdateBattleTime();
+        }
+    }
+
+    private void UpdateBattleTime()
+    {
+        _battleTime += Time.deltaTime;
+        
+        int min = (int)BattleTime / 60;
+        int sec = (int)BattleTime % 60;
+        string time = string.Format("{0:D2}:{1:D2}", min, sec);
+        
+        Debug.Log($"{time}");
+    }
+
     public int ChangeSpeed()
     {
         if (pauseCount == 0)
@@ -59,5 +82,6 @@ public class TimeManager : BaseManager<TimeManager>
         saveTimeScale = timeScaleSpeeds[0];
         pauseCount = 0;
         Time.timeScale = timeScaleSpeeds[0];
+        _battleTime = 0f;
     }
 }
