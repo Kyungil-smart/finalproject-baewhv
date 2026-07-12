dbd34f6481617456319545c2cbbfff55336d3197
baewhv <baewhv@naver.com>
2026-07-12 18:09:58 +0900

    [FIX] : UIGlow 대신 Tutorial helper로 명칭 변경

--- HCDProject/Assets/Scripts/UI/TutorialHelper.cs
+++ HCDProject/Assets/Scripts/UI/TutorialHelper.cs
@@ -2,7 +2,7 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIGlow : MonoBehaviour
+public class TutorialHelper : MonoBehaviour
 {
     [SerializeField]private Image glowImage;
 
