033859b739d60ca659f046e3fdac56100a1edbc6
baewhv <baewhv@naver.com>
2026-07-13 12:36:45 +0900

    [FIX] : 아카이브 챕터 제한, 배경 및 캐릭터 스프라이트 테이블 연동

--- HCDProject/Assets/Scripts/UI/StoryButtonUI.cs
+++ HCDProject/Assets/Scripts/UI/StoryButtonUI.cs
@@ -7,11 +7,27 @@ public class StoryButtonUI : MonoBehaviour
 {
     [SerializeField] private Button button;
     [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Image background;
+    [SerializeField] private Image mainSprite;
 
     public void SetButton(string inText, UnityAction action)
     {
         text.text = inText;
         button.onClick.AddListener(action);
+    }
+
+    public void SetImage(string bg, string main)
+    {
+        if (background)
+            background.sprite = Service.Get<ResourcesManager>().GetSprite(bg, handle =>
+            {
+                background.sprite = handle;
+            });
+        if (mainSprite)
+            mainSprite.sprite = Service.Get<ResourcesManager>().GetSprite(main, handle =>
+            {
+                mainSprite.sprite = handle;
+            });
     }
 
 }
